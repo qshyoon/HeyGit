66d26bbb180feb2b92eb2004db9ebe66274e27a7
Victoria Babasa <202110498@fit.edu.ph>
2021-09-29 10:50:14 +0800

    Added company

--- HeyGit.cpp
+++ HeyGit.cpp
@@ -10,7 +10,10 @@ int main() {
     cout << "Loona 1/3, Odd Eye Circle, yyxy \n\n";
 
     cout << "Members: \n";
-    cout << "Heejin, Hyunjin, Haseul, Yeojin, ViVi, Kim Lip, Jinsoul, Choerry, Yves, Chuu, GoWon, Olivia Hye \n";
+    cout << "Heejin, Hyunjin, Haseul, Yeojin, ViVi, Kim Lip, Jinsoul, Choerry, Yves, Chuu, GoWon, Olivia Hye \n\n";
+
+    cout << "Company: \n";
+    cout << "Blockberry Creative KR. \n";
 
     return 0;
 }
