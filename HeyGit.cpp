30782f7b60ee26f65ec96666822310d9b2010202
Victoria Babasa <202110498@fit.edu.ph>
2021-09-29 10:28:23 +0800

    Added sub-units and members

--- HeyGit.cpp
+++ HeyGit.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main() {
     cout << "Hey Github!" << endl;
 
-    cout << "Welcome to Loona";
+    cout << "Welcome to Loona \n\n";
+
+    cout << "Sub-units: \n";
+    cout << "Loona 1/3, Odd Eye Circle, yyxy \n\n";
+
+    cout << "Members: \n";
+    cout << "Heejin, Hyunjin, Haseul, Yeojin, ViVi, Kim Lip, Jinsoul, Choerry, Yves, Chuu, GoWon, Olivia Hye \n";
 
     return 0;
 }
